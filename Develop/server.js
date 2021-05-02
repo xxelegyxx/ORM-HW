d2d4e8887405227ee6a73ccc78f637f4dc679dd8
Joey <joewoodnyc@gmail.com>
2021-05-01 23:53:59 -0400

    UNFINISHED COMMIT CANT START SERVER WITH ENV

--- Develop/server.js
+++ Develop/server.js
@@ -2,22 +2,20 @@ const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
 const sequelize = require('./config/connection');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: '/.env'});
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
-});
-
 // sync sequelize models to the database, then turn on the server
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => 
-//   console.log(`App listening on port ${PORT}!`);
-// };
-
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
